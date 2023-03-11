af767ab376a00caf97782a4c71918b935538bb35
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-03-11 17:54:57 -0300

    plus commite

--- Api/server.js
+++ Api/server.js
@@ -1,31 +1,44 @@
+//Manejo de servidor
 import express from "express";
 import morgan from "morgan";
-import { Server as SocketServer} from "socket.io";
-import http from 'http'
-import cors from 'cors'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import http from "http";
+import cors from "cors";
+//Conexion a traves de socket
+import { Server as SocketServer } from "socket.io";
+//Dotenv
+import * as dotenv from "dotenv";
+dotenv.config();
 
-
+//manipulacion/generacion de el servidor
 const app = express();
-const server = http.createServer(app)
-const io = new SocketServer(server,{
-  cors:{
-    origin: '*'
-  }
-})
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-const PORT= process.env.PORT || 8080
+//configuracion del puerto
+const PORT = process.env.PORT || 8080;
 
+//Middlewares
 app.use(cors());
 app.use(morgan("dev"));
 
-io.on('connection',(socket)=>{
+io.on("connection", (socket) => {
   console.log(`User Conected.
-  id: ${socket.id}`)
-  console.log(`Un usuario se conecto`)
-})
+  id: ${socket.id}`);
+
+  socket.on("message", (message) => {
+    // console.log(`user: ${socket.id} \t message: ${message}`)
+    console.log(`message: ${message}`);
+    socket.broadcast.emit("message", {
+      body:message,
+      from:socket.id   
+    }); //atrapa y emite los mensajes a todos los usuarios
+  });
+});
 
 server.listen(PORT, () => {
-  console.log(`Server on port http://localhost:${PORT}/`); 
+  console.log(`Server on port http://localhost:${PORT}/`);
 });
--- clientChat/src/App.jsx
+++ clientChat/src/App.jsx
@@ -1,15 +1,49 @@
-import './App.css'
-import io from "socket.io-client"
+import "./App.css";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
 
-const socket = io('http://localhost:3001/');
-
+const socket = io("http://localhost:3001/");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messages,setMessages] = useState([{
+    body:"message test",
+    from: "user1"
+  }]);
+
+
+  useEffect(() => {
+    const receiveMessage = message =>{
+      // console.log(message)
+      setMessages([...messages,message])
+    }
+    socket.on("message", receiveMessage); //escucha del evento 
+    
+    return ()=>{
+      socket.off("message", receiveMessage); //desuscribe el evento
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", message);
+    setMessage('')
+  };
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setMessage(e.target.value)} value={message}/>
+        <button>send</button>
+      </form>
+      {
+      messages.map(message =>(
+      <div>
+        <p>{message.from}:{message.body}</p>
+      </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
